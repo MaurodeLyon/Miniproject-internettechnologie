3da3628d359f6cc95d8369e9f342e7f8bf084500
Arthur Brink <arthur.brink1@gmail.com>
2017-03-02 21:34:50 +0100

    chartjs

--- miniOpdracht/src/app/components/statistics.component.ts
+++ miniOpdracht/src/app/components/statistics.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import Chart from 'chart.js'
 
 @Component({
   selector: 'statistics',
@@ -7,4 +8,6 @@ import {Component} from '@angular/core';
 
 export class StatisticsComponent {
 
+
+  constructor() {};
 }
