b3bb81d31a1613dc01ad482b89c8fab28a2d68c8
Mauro de Lyon <maurodelyon@gmail.com>
2017-03-02 21:06:02 +0100

    person service

--- miniOpdracht/src/app/components/detail.component.ts
+++ miniOpdracht/src/app/components/detail.component.ts
@@ -1,10 +1,26 @@
 import {Component} from '@angular/core';
+import  {PostsService} from '../services/posts.service';
 
 @Component({
   selector: 'detail',
-  template: `<h1>Detail page</h1>`
+  templateUrl: './detail.component.html',
+  providers: [PostsService]
 })
 
 export class DetailComponent {
+  persons: Person[];
+  name: string;
 
+  constructor(private  postsService: PostsService) {
+    this.persons = this.postsService.getPosts();
+    this.name = 'TestName';
+  }
+}
+
+interface Person {
+  id: number
+  first_name: string
+  last_name: string
+  age: number
+  gender: string
 }
