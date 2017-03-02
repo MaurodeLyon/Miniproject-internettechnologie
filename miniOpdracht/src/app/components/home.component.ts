a3b88fb82ee1f79b5c219a03182c65e934c41e26
Arthur Brink <arthur.brink1@gmail.com>
2017-03-02 21:08:38 +0100

    homepage v1

--- miniOpdracht/src/app/components/home.component.ts
+++ miniOpdracht/src/app/components/home.component.ts
@@ -2,9 +2,15 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'home',
-  template: `<h1>Home page</h1>`
+  template: `<h1>Home page</h1>
+            <div>
+                  <img [src]="'http://vignette1.wikia.nocookie.net/sanicsource/images/e/e3/Sonicwut.jpg/revision/latest?cb=20160121233101The'" />
+            </div>
+            <h3>huehuehuehuehuegheg</h3>
+            `
 })
 
 export class HomeComponent {
 
+  constructor() {};
 }
