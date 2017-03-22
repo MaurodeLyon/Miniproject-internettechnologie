3f3c2765e5f5bf91d2dcc830dbe2dfcdaf6996b0
Arthur Brink <arthur.brink1@gmail.com>
2017-03-22 14:33:31 +0100

    no message

--- statusPotestatem/src/app/pastConsumption/pastConsumption.component.ts
+++ statusPotestatem/src/app/pastConsumption/pastConsumption.component.ts
@@ -14,29 +14,27 @@ export class pastConsumptionComponent {
   range:number=12;
 
 
-  // public lineChartData12: any;
-  // public lineChartLabels12: Array<any>;
-  public lineChartData12:Array<any> = [
-    {data: [0], label: 'Series A'},
-    {data: [0], label: 'Series B'}
-  ];
-  public lineChartLabels12:Array<any>;
 
 
 
-  public lineChartData24: any;
-  public lineChartLabels24: Array<any>;
 
+  public arthurLineChartLabels12: Array<any>;
+  public mauroLineChartLabels12: Array<any>;
 
-  public lineChartData48: any;
-  public lineChartLabels48: Array<any>;
+  public arthurLineChartLabels24: Array<any>;
+  public mauroLineChartLabels24: Array<any>;
 
+  public arthurLineChartLabels48: Array<any>;
+  public mauroLineChartLabels48: Array<any>;
 
-  public lineChartDataWeek: any;
-  public lineChartLabelsWeek: Array<any>;
+  public arthurLineChartData12: Array<any>;
+  public mauroLineChartData12: Array<any>;
 
+  public arthurLineChartData24: Array<any>;
+  public mauroLineChartData24: Array<any>;
 
-  //public arthurLineChartLabels: Array<any>;
+  public arthurLineChartData48: Array<any>;
+  public mauroLineChartData48: Array<any>;
 
 
   public lineChartOptions: any = {
@@ -57,115 +55,74 @@ export class pastConsumptionComponent {
 
 
   constructor(private postsService: PostsService) {
-    // this.postsService.getArthurMeasurements().subscribe(posts => {
-    //   this.arthurMeasurements = posts;
-    //   //console.log(this.measurements.results[0].ticks);
-    //   this.genArthurLineData();
-    //
-    // });
+    this.postsService.getArthurMeasurements().subscribe(posts => {
+      this.arthurMeasurements = posts;
+      //console.log(this.measurements.results[0].ticks);
+      this.genArthurLineData();
+
+    });
     //
     this.postsService.getMauroMeasurements().subscribe(posts => {
       this.mauroMeasurements = posts;
-      this.postsService.getArthurMeasurements().subscribe(posts => {
-        this.arthurMeasurements = posts;
-        // this.postsService.getMauroMeasurements().subscribe(posts => {
-        //     this.mauroMeasurements = posts;
-        // });
-        //this.gen12HourLineData();
-       // console.log(this.arthurMeasurements);
-
-        this.gen12HourLineData();
-      });
-
+      this.genMauroLineData()
 
     });
 
 
   }
+  // public setRange(value): void{
+  //   this.range=value;
+  //   this.arthurLineChartLabels.length=this.range;
+  //   //this.arthurLineChartLabels.push(22);
+  //   //this.mauroLineChartLabels.push(22);
+  //   this.genMauroLineData();this.genArthurLineData();
+  //   console.log(value);
+  // }
 
-  public gen12HourLineData(): void {
-
-    this.lineChartData12 = [
+  public genArthurLineData12(): void {
+    this.arthurLineChartData12 = [
       {
-        label: 'Arthur',
-        data: []
-      },
-      {
-        label: 'Mauro',
+        label: 'Scatter Dataset',
         data: []
       }
     ];
-    this.lineChartLabels12 = [0,0];
+    this.arthurLineChartLabels12 = [];
 
-    //this.lineChartLabels12.push();
-   // console.log(this.lineChartLabels12[0].data[0]);
-
-   // if (this.arthurMeasurements != null && this.mauroMeasurements != null) {
-      var lengthArthur = this.arthurMeasurements.results.length;
-      var lengthMauro = this.mauroMeasurements.results.length;
-      for (var i = 12; i > 1; i--) {
-        var val = this.arthurMeasurements.results[lengthArthur - i].ticks;
+    if (this.arthurMeasurements != null) {
+      var length = this.arthurMeasurements.results.length;
+      for (var i = this.range; i > 1; i--) {
+        var val = this.arthurMeasurements.results[length - i].ticks;
         val = val / 187.5;
         val = val * 1000;
-        this.lineChartData12[0].data.push(val);
+        this.arthurLineChartData12[0].data.push(val);
+
+        this.arthurLineChartLabels12.push((this.arthurMeasurements.results[length - i].hour));
+        console.log(this.arthurLineChartData12[0].data[i]);
+      }
+    }
+  }
+
+  public genMauroLineData(): void {
+    //console.log(this.measurements.length);
+    //this.mauroLineChartData[0].data.clear();
+    this.mauroLineChartData = [
+      {
+        label: 'Scatter Dataset',
+        data: []
+      }
+    ];
+    this.mauroLineChartLabels = [];
 
 
-
-        this.lineChartData12[1].data.push(this.mauroMeasurements.results[lengthMauro - i].ticks);
-        this.lineChartLabels12[12-i](this.mauroMeasurements.results[lengthMauro - i].hour);
-
-
+    if (this.mauroMeasurements != null) {
+      var length = this.mauroMeasurements.results.length;
+      for (var i = this.range; i > 1; i--) {
+        this.mauroLineChartData[0].data.push(this.mauroMeasurements.results[length - i].ticks);
+        this.mauroLineChartLabels.push(this.mauroMeasurements.results[length - i].hour);
+        console.log(this.mauroLineChartData[0].data[i]);
       }
 
-
-
-    //}
-
+      //console.log(this.mauroLineChartData[0].data[0]);
+    }
   }
-  // public genArthurLineData(): void {
-  //   this.arthurLineChartData = [
-  //     {
-  //       label: 'Scatter Dataset',
-  //       data: []
-  //     }
-  //   ];
-  //   this.arthurLineChartLabels = [];
-  //
-  //   if (this.arthurMeasurements != null) {
-  //     var length = this.arthurMeasurements.results.length;
-  //     for (var i = this.range; i > 1; i--) {
-  //       var val = this.arthurMeasurements.results[length - i].ticks;
-  //       val = val / 187.5;
-  //       val = val * 1000;
-  //       this.arthurLineChartData[0].data.push(val);
-  //       this.arthurLineChartLabels.push(this.arthurMeasurements.results[length - i].hour);
-  //       console.log(this.arthurLineChartData[0].data[i]);
-  //     }
-  //
-  //     //console.log(this.mauroLineChartData[0].data[0]);
-  //   }
-  // }
-  //
-  // public genMauroLineData(): void {
-  //   //console.log(this.measurements.length);
-  //   //this.mauroLineChartData[0].data.clear();
-  //   this.mauroLineChartData = [
-  //     {
-  //       label: 'Scatter Dataset',
-  //       data: []
-  //     }
-  //   ];
-  //   this.mauroLineChartLabels = [];
-  //
-  //   if (this.mauroMeasurements != null) {
-  //     var length = this.mauroMeasurements.results.length;
-  //     for (var i = this.range; i > 1; i--) {
-  //       this.mauroLineChartData[0].data.push(this.mauroMeasurements.results[length - i].ticks);
-  //       this.mauroLineChartLabels.push(this.mauroMeasurements.results[length - i].hour);
-  //       console.log(this.mauroLineChartData[0].data[i]);
-  //     }
-  //
-  //     //console.log(this.mauroLineChartData[0].data[0]);
-  //   }
-  // }
 }
