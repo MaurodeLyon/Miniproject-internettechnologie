c2f9ecbd3e5c2953bd1624ecccf9004d716313e4
Arthur Brink <arthur.brink1@gmail.com>
2017-03-22 16:17:20 +0100

    finished past consumption

--- statusPotestatem/src/app/pastConsumption/pastConsumption.component.ts
+++ statusPotestatem/src/app/pastConsumption/pastConsumption.component.ts
@@ -10,27 +10,94 @@ import {LineChart} from "../charts/LineChart";
 
 })
 export class pastConsumptionComponent {
-  MauroLineChart12Hour: MauroLineChart12Hour;
+  mauroMeasurements: data;
+  arthurMeasurements: data;
+
+  mauro12: HourlyLineChart;
+  mauro24: HourlyLineChart;
+  mauro48: HourlyLineChart;
+
+  arthur12: HourlyLineChart;
+  arthur24: HourlyLineChart;
+  arthur48: HourlyLineChart;
 
   constructor(private postsService: PostsService) {
     this.postsService.getMauroMeasurements().subscribe(posts => {
+      this.mauroMeasurements = posts;
+      this.mauro12 = new HourlyLineChart(12,false);
+      this.mauro24 = new HourlyLineChart(24,false);
+      this.mauro48 = new HourlyLineChart(48,false);
 
+
+      console.log(posts);
+      this.mauro12.genListData(posts);
+      this.mauro24.genListData(posts);
+      this.mauro48.genListData(posts);
+
+
+    });
+
+    this.postsService.getArthurMeasurements().subscribe(posts => {
+      this.arthurMeasurements = posts;
+
+
+      this.arthur12 = new HourlyLineChart(12,true);
+      this.arthur24 = new HourlyLineChart(24,true);
+      this.arthur48 = new HourlyLineChart(48,true);
+
+      this.arthur12.genListData(posts);
+      this.arthur24.genListData(posts);
+      this.arthur48.genListData(posts);
     });
   }
 }
 
-class MauroLineChart12Hour implements LineChart {
-  lineChartData: Array<any>;
-  lineChartLabels: Array<any>;
-  lineChartOptions: any;
-  lineChartColors: Array<any>;
-  lineChartLegend: boolean;
-  lineChartType: string = 'line';
+class HourlyLineChart implements LineChart {
+  lineChartData: Array<any>= [{data: [], label: 'Watt per hour'}];
+  lineChartLabels: Array<any>=[];
+  public lineChartOptions: any = {
+    responsive: true
+  };
+  public lineChartLegend: boolean = true;
+  public lineChartType: string = 'line';
+  public lineChartColors: Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }];
+  range:number=0;
+  dialMeter:boolean=false;
+  constructor(destinedRange:number,dialMeter:boolean)
+  {
+    this.range=destinedRange;
+    this.dialMeter=dialMeter;
+  }
+  public genListData(values:data):void {
+    if (values != null) {
+      var length = values.results.length;
+      for (var i = this.range; i > 1; i--) {
+        if(this.dialMeter)
+        {
+          let val = values.results[length - i].ticks;
+          val = val / 187.5;
+          val = val * 1000;
+          this.lineChartData[0].data.push(val);
+        }
+        else{
+          this.lineChartData[0].data.push(values.results[length - i].ticks);
+        }
 
+        this.lineChartLabels.push(values.results[length - i].hour);
+      }
+    }
+  }
   chartClicked(e: any): void {
   }
 
   chartHovered(e: any): void {
   }
-
 }
--- statusPotestatem/src/app/trend/trend.component.ts
+++ statusPotestatem/src/app/trend/trend.component.ts
@@ -1,8 +1,76 @@
 import { Component } from '@angular/core';
+import  {PostsService} from '../services/posts.service';
+import {data} from '../services/Data'
+import {LineChart} from "../charts/LineChart";
 
 @Component({
   selector: 'app-root',
   templateUrl: './trend.component.html'
 })
 export class trendComponent {
+  mauroMeasurements: data;
+  arthurMeasurements: data;
+
+  mauroTrend: TrendLineChart;
+  arthurTrend: TrendLineChart;
+
+  constructor(private postsService: PostsService) {
+    this.postsService.getMauroMeasurements().subscribe(posts => {
+      this.mauroMeasurements=posts;
+    });
+    this.postsService.getArthurMeasurements().subscribe(posts => {
+      this.arthurMeasurements=posts;
+    });
+
+  }
 }
+class TrendLineChart implements LineChart {
+  lineChartData: Array<any>= [{data: [], label: 'Watt per hour'}];
+  lineChartLabels: Array<any>=[];
+  public lineChartOptions: any = {
+    responsive: true
+  };
+  public lineChartLegend: boolean = true;
+  public lineChartType: string = 'line';
+  public lineChartColors: Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }];
+  dialMeter:boolean=false;
+  constructor(dialMeter:boolean)
+  {
+    this.dialMeter=dialMeter;
+  }
+  public genListData(values:data):void {
+    if (values != null) {
+      var length = values.results.length;
+      var currentDay=values.results[length-1].day;
+
+      for (var i = 24; i > 1; i--) {
+        if(this.dialMeter)
+        {
+          let val = values.results[length - i].ticks;
+          val = val / 187.5;
+          val = val * 1000;
+          this.lineChartData[0].data.push(val);
+        }
+        else{
+          this.lineChartData[0].data.push(values.results[length - i].ticks);
+        }
+        this.lineChartLabels.push(values.results[length - i].hour);
+      }
+    }
+  }
+  chartClicked(e: any): void {
+  }
+
+  chartHovered(e: any): void {
+  }
+}
+
+
